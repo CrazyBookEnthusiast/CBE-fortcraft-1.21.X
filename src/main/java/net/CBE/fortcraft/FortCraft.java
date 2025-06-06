0c8b65858e755d061608203d2de26c480249e68e
CrazyBookEnthusiast <crazybookenthusiast@gmail.com>
2025-06-06 22:31:46 +0530

    Made some chenges

--- src/main/java/net/CBE/fortcraft/FortCraft.java
+++ src/main/java/net/CBE/fortcraft/FortCraft.java
@@ -1,5 +1,7 @@
 package net.CBE.fortcraft;
 
+import net.CBE.fortcraft.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -21,7 +23,7 @@ import net.neoforged.neoforge.event.server.ServerStartingEvent;
 @Mod(FortCraft.MOD_ID)
 public class FortCraft
 {
-    public static final String MOD_ID = "cbefortcraft";
+    public static final String MOD_ID = "fortcraft";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
@@ -34,6 +36,9 @@ public class FortCraft
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
+
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -48,7 +53,13 @@ public class FortCraft
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey()== CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
 
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call

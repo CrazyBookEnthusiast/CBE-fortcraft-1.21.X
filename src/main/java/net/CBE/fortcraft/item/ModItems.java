f2111134c6e13a8136cfb18552270c18b9fc03cb
CrazyBookEnthusiast <crazybookenthusiast@gmail.com>
2025-06-08 19:32:22 +0530

    Added tooltips

--- src/main/java/net/CBE/fortcraft/item/ModItems.java
+++ src/main/java/net/CBE/fortcraft/item/ModItems.java
@@ -3,12 +3,17 @@ package net.CBE.fortcraft.item;
 import net.CBE.fortcraft.FortCraft;
 import net.CBE.fortcraft.block.ModBlocks;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemNameBlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(FortCraft.MOD_ID);
@@ -20,7 +25,13 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
 
     public static final DeferredItem<Item> SLURP_BERRIES = ITEMS.register("slurp_berries",
-            () -> new ItemNameBlockItem(ModBlocks.SLURP_BERRY_BUSH.get(), new Item.Properties().food(ModFoodProperties.SLURP_BERRIES)));
+            () -> new ItemNameBlockItem(ModBlocks.SLURP_BERRY_BUSH.get(), new Item.Properties().food(ModFoodProperties.SLURP_BERRIES)){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.fortcraft.slurp_berries"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
